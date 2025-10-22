6d055f97f7cb6480a5bc1617083286a1f5a82e31
Titano93 <f.lager1993@gmail.com>
2025-10-22 08:57:26 +0200

    Revert "keeping one message"
    This reverts commit f3fef0b32bc5c4820456aefdc3bdbe3f3554c735.

--- HelloWorldApp/Program.cs
+++ HelloWorldApp/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Titano1993! One more modification!");
+Console.WriteLine("Hello, Titano1993! One more modification!");
+Console.WriteLine("Hello, Titano1993! One more modification!");
+Console.WriteLine("Hello, Titano1993! One more modification!");
+Console.WriteLine("Hello, Titano1993! One more modification!");
+Console.WriteLine("Hello, Titano1993! One more modification!");
+Console.WriteLine("Hello, Titano1993! One more modification!");
